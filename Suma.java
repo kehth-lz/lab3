24b4b16c66ba0afd6b7c8fe4b397082d46589cfa
Jose LuisQ <jslqc77@gmail.com>
2026-03-13 15:23:11 -0400

    Se realizo cambios en el codigo

--- Suma.java
+++ Suma.java
@@ -3,7 +3,7 @@ public class Suma {
         int numero1 = 5;
         int numero2 = 10;
         int resultado = numero1 + numero2;
-        
+        int a = 0;
         System.out.println("La suma es: " + resultado);
     }
 }
--- operaciones.py
+++ operaciones.py
@@ -1,5 +1,5 @@
 numero1 = 5
 numero2 = 10
 resultado = numero1 + numero2
-
+a = 0
 print("La suma es:", resultado)
