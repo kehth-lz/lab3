b26ab78afd531c6e89430094e273dc0b9866d658
Jose LuisQ <jslqc77@gmail.com>
2026-03-13 15:12:15 -0400

    Se desarrolló el código en cada lenguaje

---
+++ Suma.java
@@ -0,0 +1,9 @@
+public class Suma {
+    public static void main(String[] args) {
+        int numero1 = 5;
+        int numero2 = 10;
+        int resultado = numero1 + numero2;
+        
+        System.out.println("La suma es: " + resultado);
+    }
+}
---
+++ operaciones.py
@@ -0,0 +1,5 @@
+numero1 = 5
+numero2 = 10
+resultado = numero1 + numero2
+
+print("La suma es:", resultado)
