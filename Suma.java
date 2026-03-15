34b7947ef528f671436a4386eaa2d86b1f209600
Jose LuisQ <jslqc77@gmail.com>
2026-03-14 21:36:03 -0400

    Se añadio una nueva linea al codigo suma

--- Suma.java
+++ Suma.java
@@ -4,6 +4,8 @@ public class Suma {
         int numero2 = 10;
         int resultado = numero1 + numero2;
         int a = 0;
+        int z = -5;
         System.out.println("La suma es: " + resultado);
+
     }
 }
